ca51af94897bcdf5ed458c10bbc7b758609e7043
Satrajit Basu <satrajit123basu@gmail.com>
2021-01-13 14:46:04 +0530

    Base models

---
+++ learningpoc/user_management/models/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+# Learning POC Imports Section:
+from user_management.models.lappusers import LappUser
+
+__all__ = [
+    'LappUser',
+]
